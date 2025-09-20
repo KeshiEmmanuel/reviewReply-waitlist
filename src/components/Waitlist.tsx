6c2f21dfb0864c2b48583ec7d3e0f6ac4d416dc3
KeshiEmmanuel <keshichidera@gmail.com>
2025-09-20 15:01:55 -0400

    fix: Increase close button size in Waitlist component for better accessibility

--- src/components/Waitlist.tsx
+++ src/components/Waitlist.tsx
@@ -161,7 +161,7 @@ const Waitlist = () => {
                             onClick={closeAlert}
                             className="text-gray-500 hover:text-gray-700 ml-4"
                         >
-                            <X className="w-4 h-4" />
+                            <X className="w-5 h-5" />
                         </button>
                     </div>
                 </div>
